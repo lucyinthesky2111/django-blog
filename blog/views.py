3a73441e999ea634656562d4fd3908ec02157633
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-01 20:10:06 +0100

    Add code to allow users to leave comments for approval by admnin

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-    
+
+
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -27,12 +30,27 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+
     return render(
-    request,
-    "blog/post_detail.html",
-    {
-        "post": post,
-        "comments": comments,
-        "comment_count": comment_count,
-    },
-)
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
+    )
