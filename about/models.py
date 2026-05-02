f9ddb8e8e4577d777a576ba5d515f7a54df9ec77
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-02 16:47:58 +0100

    Add code to display images uploaded by both users and admin

--- about/models.py
+++ about/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-
-# Create your models here.
+from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     def __str__(self):
         return self.title
-
 
 class CollaborateRequest(models.Model):
     name = models.CharField(max_length=200)
--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about', 
 ]
