8ce4d02237f05845cb8ab5fb9b8c61b5ce43c5eb
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-02 16:02:38 +0100

    Add code to enable serving of image files via Cloudinary

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
